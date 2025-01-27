f6c4c104a841a6aafe3b1cd177b3d6209e013e54
byubraden <byubraden26@gmail.com>
2025-01-27 12:43:42 -0700

    Madi Changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Tic-Tac-Toe!");
+//  the game board (3x3 grid)
+char[,] board = {
+    { '1', '2', '3' },
+    { '4', '5', '6' },
+    { '7', '8', '9' }
+};
+// These are variables to keep track of the game
+bool gameWon = false;
+int turns = 0;
+char currentPlayer = 'X';
+// Main game loop (I did not finish this all the way)
+while (!gameWon && turns < 9)
+{
+    PrintBoard(board);
+    // Ask the current player for their choice
+    Console.WriteLine($"Player {currentPlayer}, choose a position: ");
+    string input = Console.ReadLine();
+    if (int.TryParse(input, out int position) && position >= 1 && position <= 9)
+    {
+    }
 
-// Test to see 
+}
